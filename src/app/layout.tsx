ca2a07ab0dd88ceb871023a6b4af772b97b22d91
ShubhamKr-047 <shubhamkumar.cse047@gmail.com>
2025-08-25 01:19:59 +0530

    v1.0.6 metadata fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,19 +60,10 @@ export const metadata: Metadata = {
     lastName: USER.lastName,
     username: USER.username,
     gender: USER.gender,
-    images: [
-      {
-        url: SITE_INFO.ogImage,
-        width: 1200,
-        height: 630,
-        alt: SITE_INFO.name,
-      },
-    ],
   },
   twitter: {
-    card: "summary_large_image",
+    card: "summary",
     creator: "@iamncdai", // Twitter username
-    images: [SITE_INFO.ogImage],
   },
   icons: {
     icon: [
